70cc29eec731dd5d63ecb2f00b421732a32c29a4
Praveen <tkmpraveens@gmail.com>
2023-02-11 16:59:04 +0530

    Read me file and license has been added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "./Styles/main.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-reportWebVitals();
+root.render(<App />);
